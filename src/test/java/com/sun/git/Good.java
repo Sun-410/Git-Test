abda1a64b7288386eed855ee8e22483c3af18ef5
Sun <Late_Autumn_Sun@aliyun.com>
2021-01-30 14:39:28 +0800

    hox_fix

--- src/test/java/com/sun/git/Good.java
+++ src/test/java/com/sun/git/Good.java
@@ -3,6 +3,6 @@ package com.sun.git;
 public class Good {
 	public static void main(String[] args) {
 		System.out.println("冲突解决");
-		System.out.println("分支创建：hot_fix");
+		System.out.println("分支创建：Hot_FIX");
 	}
 }
