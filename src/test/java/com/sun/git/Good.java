fe8711216f96cb932427db970e8a1222e58e91bc
Sun <Late_Autumn_Sun@aliyun.com>
2021-01-30 14:34:37 +0800

    2:34

--- src/test/java/com/sun/git/Good.java
+++ src/test/java/com/sun/git/Good.java
@@ -2,7 +2,7 @@ package com.sun.git;
 
 public class Good {
 	public static void main(String[] args) {
-		System.out.println("�״�ѧϰ�����ָ��");
 		System.out.println("冲突解决");
+		System.out.println("分支创建：hot_fix");
 	}
 }
